7a3fda7d9386dad65c2b1b6fd6f6033af41d12e0
Cheruiyot Emmanuel <emmanuel2kipngeno@gmail.com>
2022-08-22 00:01:07 +0300

    Initial commit

---
+++ Assets/PropellerRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotate : MonoBehaviour
+{
+
+    //private float degreesPerSecond  = 360.0f;
+    private float speed = 720.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
